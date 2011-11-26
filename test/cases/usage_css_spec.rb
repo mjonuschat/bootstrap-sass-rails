30964195f2af140281faf1f61006ff2c7e015390
Morton Jonuschat <yabawock@gmail.com>
2011-11-26 18:44:53 +0100

    Make it work like the documentation says

--- test/cases/usage_css_spec.rb
+++ test/cases/usage_css_spec.rb
@@ -7,7 +7,7 @@ class UsageCssSpec < Bootstrap::Sass::Rails::Spec
     let(:app_css) { dummy_asset('application.css') }
 
     it 'will render main bootstrap.less file and all included modules' do
-      app_css.must_include 'Bootstrap @VERSION',          'From boostrap.css.scss'
+      app_css.must_include 'Bootstrap @VERSION',          'From bootstrap.css.scss'
       app_css.must_include 'Forms.scss',                  'From forms.scss'
       app_css.must_include 'Snippets of reusable CSS',    'From mixins.scss'
       app_css.must_include 'Patterns.scss',               'From patterns.scss'
@@ -25,8 +25,6 @@ class UsageCssSpec < Bootstrap::Sass::Rails::Spec
 
   describe 'individual.scss' do
 
-    before { dummy_config.assets.compress = true }
-
     let(:individual_css) { dummy_asset('individual.css') }
 
     it 'will render bootstrap variables and mixins' do
@@ -34,6 +32,27 @@ class UsageCssSpec < Bootstrap::Sass::Rails::Spec
       my_button_line.must_include '-webkit-border-radius: 10px'
       my_button_line.must_include '-moz-border-radius: 10px'
       my_button_line.must_include 'border-radius: 10px'
+    end
+
+  end
+
+  describe 'sprockets.css' do
+
+    let(:sprockets_css) { dummy_asset('sprockets.css') }
+
+    it 'will render main bootstrap.css file and all included modules' do
+      sprockets_css.must_include 'Bootstrap @VERSION',          'From boostrap.css.scss'
+      sprockets_css.must_include 'Forms.scss',                  'From forms.scss'
+      sprockets_css.must_include 'Snippets of reusable CSS',    'From mixins.scss'
+      sprockets_css.must_include 'Patterns.scss',               'From patterns.scss'
+      sprockets_css.must_include 'Scaffolding',                 'From scaffolding.scss'
+      sprockets_css.must_include 'Tables.scss',                 'From tables.scss'
+      sprockets_css.must_include 'Typography.scss',             'From type.scss'
+      sprockets_css.must_include 'Variables.scss',              'From variables.scss'
+    end
+
+    it 'must include basic css afterward' do
+      sprockets_css.must_include '#other-css { color: red; }',  'From our code afterward.'
     end
 
   end
